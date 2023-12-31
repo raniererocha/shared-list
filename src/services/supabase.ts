2fe80dd8bbafe58ea120473980cfbc7eb0adcb3d
Raniere Rocha <rlsmont123@hotmail.com>
2023-12-31 18:08:46 -0300

    fix: send all items

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const { SUPA_URL, SUPA_KEY } = process.env as Record<string, string>
+const { NEXT_PUBLIC_SUPA_URL, NEXT_PUBLIC_SUPA_KEY } = process.env as Record<
+  string,
+  string
+>
 
-export const client = createClient(
-  'https://xvaiutmgijfsshkhxppl.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2YWl1dG1naWpmc3Noa2h4cHBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQwNTAxMDcsImV4cCI6MjAxOTYyNjEwN30.E605Noeq8Dsr89XYlFzREhrKzBJvDYof2yGVb_5Q1AU',
-)
+export const client = createClient(NEXT_PUBLIC_SUPA_URL, NEXT_PUBLIC_SUPA_KEY)
